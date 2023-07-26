abcc119541be43aaa3d7536bdbbae7176824c268
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-26 12:17:13 +0300

    writes html for right container

--- src/components/landing_page/LandingPage.js
+++ src/components/landing_page/LandingPage.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import MyHeader from '../header/MyHeader'
 import './landing_page.css'
+import Illustration from '../../images/illustration-mockups.svg'
 
 const LandingPage = () => {
   return (
     <div className="main-container">
-        <MyHeader/>
+      <MyHeader />
+      <main>
+        <div className="left-container">
+          <img src={Illustration} alt="illustration" />
+        </div>
+        <div className="right-container">
+          <p>Build The Community</p>
+          <p>Your fans will love</p>
+          <p>Huddle reimagines the way we build communities. You have a voice, but so does your audience. Create connections wit your users as you engage in genuine discussion.</p>
+          <button className='register-button'>Register</button>
+        </div>
+
+      </main>
     </div>
   )
 }
